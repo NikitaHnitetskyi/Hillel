1b3f145685cb09feb1ad27bffb3d842b6fd4f080
Nikita_Hnitetskyi <n.gnitetsky@gmail.com>
2023-03-14 12:49:01 +0200

    Create Banana!
    add fileds and properties

---
+++ Лекція 2.ООП/Banana.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Apple
+{
+    internal class Banana : BaseFruit
+    {
+        //поля
+        private int numberofpips;
+
+        //свойства
+        public int Numberofpips => numberofpips;
+    }
+}
